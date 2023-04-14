c69b2630598527140300a0620bc417d810897796
jungang11 <94428775+jungang11@users.noreply.github.com>
2023-04-14 20:41:15 +0900

    feat. Bingo.Map클래스 생성
    Map 클래스 생성.
    MakeMap 맵 생성 함수
    ShuffleMap 맵 숫자 섞기 함수

---
+++ BingoGame/Map.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BingoGame
+{
+    public class Map
+    {
+        // 맵 생성
+        public int[,] MakeMap()
+        {
+            int[,] bingo = new int[5, 5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    bingo[i, j] = i * 5 + j + 1;
+                }
+            }
+            ShuffleMap(bingo);
+
+            return bingo;
+        }
+
+        // 맵 랜덤으로 숫자 섞기
+        public void ShuffleMap(int[,] bingo)
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                int randNum1 = rand.Next() % 5;
+                int randNum2 = rand.Next() % 5;
+                int randNum3 = rand.Next() % 5;
+                int randNum4 = rand.Next() % 5;
+
+                int temp = bingo[randNum1, randNum2];
+                bingo[randNum1, randNum2] = bingo[randNum3, randNum4];
+                bingo[randNum3, randNum4] = temp;
+            }
+        }
+    }
+}
--- BingoGame/Program.cs
+++ BingoGame/Program.cs
@@ -1,7 +1,16 @@
 ﻿namespace BingoGame
 {
+    /*
+    5x5 판을 생성하고 랜덤한 숫자를 배치한다.
+    원하는 숫자 입력시 해당 숫자는 특수기호로 바꾼다.
+    생성한 숫자외의 다른 수를 입력할 수 없다.(예외처리)
+    종료 조건은 빙고 3줄 이상시 종료한다.
+     */
+
     internal class Program
     {
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
