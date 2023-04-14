bfdf6da924897fd33435ad1a4e085b6cbc02df9c
jungang11 <94428775+jungang11@users.noreply.github.com>
2023-04-14 20:29:24 +0900

    docs. UpDownGame Init
    Up Down 게임 프로젝트 생성

---
+++ UpDownGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UpDownGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
