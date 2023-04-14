1511847f284069f23aa44af21ffb3faeebd697e0
jungang11 <94428775+jungang11@users.noreply.github.com>
2023-04-14 21:40:45 +0900

    docs. SlidePuzzle Init
    슬라이드퍼즐 생성

---
+++ SlidePuzzle/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SlidePuzzle
+{
+    internal class Program
+    {
+        /* 슬라이드 퍼즐 만들기 
+         * 5x5 판을 생성하고 랜덤한 숫자를 배치한다.
+         * 시작위치는 상관없으며 ArrowKey입력시 해당 방향으로 이동한다.
+         * 단, 밖으로 벗어날 수 없다.
+         * 아래 예시는 0이 움직이는 것으로 가정한다.
+         */
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
