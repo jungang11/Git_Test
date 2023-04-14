56aa41554ef7f8702f36ea36a0a012aadd039ceb
jungang11 <94428775+jungang11@users.noreply.github.com>
2023-04-14 21:41:52 +0900

    feat. SlidePuzzle Map Class
    슬라이드퍼즐 맵 클래스

---
+++ SlidePuzzle/Map.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlidePuzzle
+{
+    internal class Map
+    {
+        public int[,] MakeMap()
+        {
+            int[,] map = new int[5, 5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    map[i, j] = i * 5 + j + 1;
+                }
+            }
+            ShuffleMap(map);
+
+            return map;
+        }
+
+        public void ShuffleMap(int[,] map)
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                int randNum1 = rand.Next() % 5;
+                int randNum2 = rand.Next() % 5;
+                int randNum3 = rand.Next() % 5;
+                int randNum4 = rand.Next() % 5;
+
+                int temp = map[randNum1, randNum2];
+                map[randNum1, randNum2] = map[randNum3, randNum4];
+                map[randNum3, randNum4] = temp;
+            }
+        }
+    }
+}
